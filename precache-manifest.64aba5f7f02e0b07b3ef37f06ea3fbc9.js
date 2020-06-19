a9e9a84dc5aa25bda2e6be859867d86ac24b72f8
harsh <harsh710pat@gmail.com>
2020-06-20 02:04:52 +0530

    Updates

---
+++ precache-manifest.64aba5f7f02e0b07b3ef37f06ea3fbc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672b08d6efbdf40acba9cf7c15b1fcc0",
+    "url": "/rickandmorty/index.html"
+  },
+  {
+    "revision": "c2f8224d3b929b6e732e",
+    "url": "/rickandmorty/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cb4b406d055341dc5478",
+    "url": "/rickandmorty/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "c2f8224d3b929b6e732e",
+    "url": "/rickandmorty/static/js/2.8963fb3c.chunk.js"
+  },
+  {
+    "revision": "cb4b406d055341dc5478",
+    "url": "/rickandmorty/static/js/main.fa7051e7.chunk.js"
+  },
+  {
+    "revision": "a1b2f4593c2c28b002e8",
+    "url": "/rickandmorty/static/js/runtime~main.a1dd2e89.js"
+  }
+]);
